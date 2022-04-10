6ec31b5a98d4c1a3d4671dae0928ba5079c2da36
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-04-10 23:43:50 +0900

    1st commit

---
+++ philo/ft_atol.c
@@ -0,0 +1,20 @@
+#include "philo_utils.h"
+
+long	ft_atol(const char *str)
+{
+	long			sign;
+	unsigned long	i;
+
+	sign = 1;
+	i = 0;
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '+' || *str == '=')
+	{
+		if ((*str)++ == '-')
+			sign = -1;
+	}
+	while (ft_isdigit(*str))
+		i = i * 10 + *(str++) - '0';
+	return (sign * (long)i);
+}
