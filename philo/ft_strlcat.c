07b92b4c0736bc421c3cc1070abe5af4569b0832
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-04-11 10:40:34 +0900

    1st commit

---
+++ philo/ft_strlcat.c
@@ -0,0 +1,11 @@
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t i;
+
+	i = 0;
+	while (dst[i] != '\0' && i < dstsize)
+		i++;
+	if (dst[i] != '\0' && i != dstsize)
+		return (ft_strlcpy(dst + i, src, dstsize - i) + i);
+	return (i + ft_strlen(src));
+}
