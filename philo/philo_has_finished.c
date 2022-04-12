0f0dc468fa369f20ebbf6513c80360f155f5a682
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-04-12 21:23:21 +0900

    1st commit

---
+++ philo/philo_has_finished.c
@@ -0,0 +1,12 @@
+#include "philo.h"
+
+int	philo_has_finished(long time_start, long time_to_do)
+{
+	long time_no;
+
+	if ((time_now = philo_gettime()) < 0)
+		return (1);
+	if (time_now - time_start >= time_to_do)
+		return (1);
+	return (0);
+}
