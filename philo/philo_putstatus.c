b7c3e1b94f7b797729b1b680a5c1ca0167be1b9b
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-04-14 06:34:25 +0900

    update

--- philo/philo_putstatus.c
+++ philo/philo_putstatus.c
@@ -13,4 +13,21 @@ static int	get_digit(long num)
 	return (digit);
 }
 
-static
+static void	cat_number(char *buf, long num)
+{
+	int	digit;
+
+	while (*buf)
+		buf++;
+	digit = get_digit(num);
+	buf[digit] = '\0';
+	while (digit > 0)
+	{
+		buf[digit - 1 ] = (char)(num % 10L) + '0';
+		num /= 10;
+		digit--;
+	}
+}
+
+
+
