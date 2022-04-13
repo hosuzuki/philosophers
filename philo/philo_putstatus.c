bec6a3195212989c8be353419efc3ed7e1255713
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-04-14 07:07:19 +0900

    1st commit

--- philo/philo_putstatus.c
+++ philo/philo_putstatus.c
@@ -29,5 +29,27 @@ static void	cat_number(char *buf, long num)
 	}
 }
 
+void	philo_putstatus(long philo_index, long time_ms, int philo_status)
+{
+	char	buf[256];
 
-
+	buf[0] = '\0';
+	cat_number(buf, time_ms);
+	ft_strlcat(buf, " ", sizeof(buf));
+	cat_number(buf, philo_index + 1);
+	if (philo_status == PHILO_S_TAKENFORK)
+		ft_strlcat(buf, " has taken a fork\n", sizeof(buf));
+	else if (philo_status == PHILO_S_EATING)
+		ft_strlcat(buf, " is eating\n", sizeof(buf));
+	else if (philo_status == PHILO_S_SLEEPING)
+		ft_strlcat(buf, " is sleepgin\n", sizeof(buf));
+	else if (philo_status == PHILO_S_THINKING)
+		ft_strlcat(buf, " is thinking\n", sizeof(buf));
+	else if (philo_status == PHILO_S_DEAD)
+		ft_strlcat(buf, " died\n", sizeof(buf));
+	else
+		ft_strlcat(buf, " is in undefined status\n", sizeof(buf));
+	pthread_mutex_lock(&g_mutex_write);
+	write(1, buf, ft_strlen(buf));
+	pthread_mutex_unlock(&g_mutex_write);
+}
