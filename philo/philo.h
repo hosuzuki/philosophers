0292b699ef8be0a3c856cbf6e2bbfc5afe40b9d3
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-04-15 10:08:22 +0900

    mod for ft_save_argv.c

--- philo/philo.h
+++ philo/philo.h
@@ -7,6 +7,7 @@
 # include <sys/types.h>
 # include <sys/time.h>
 # include <string.h>
+# include <stdio.h>
 
 # define PHILO_WHILE_INTERVAL_USEC	200
 # define PHILO_S_TAKENFORK			1
@@ -15,7 +16,7 @@
 # define PHILO_S_THINKING			4
 # define PHILO_S_DEAD				5
 
-typedef struct	s_philo
+typedef struct	s_lst
 {
 	long		index;
 	long		n_philo;
@@ -23,32 +24,33 @@ typedef struct	s_philo
 	long		time_to_eat;
 	long		time_to_sleep;
 	long		n_to_eat;
-}				t_philo;
+	int				*fork;
+	int				*fork_rsvd_by;
+	pthread_mutex_t	*mutex_fork;
+	int				flgend;
+	pthread_mutex_t	mutex_flgend;
+	int				n_finished;
+	pthread_mutex_t	mutex_n_finished;
+	pthread_mutex_t	mutex_write;
+}	t_lst;
 
-int				*g_fork;
-int				*g_fork_rsvd_by;
-pthread_mutex_t	*g_mutex_fork;
-int				g_flgend;
-pthread_mutex_t	g_mutex_flgend;
-int				g_n_finished;
-pthread_mutex_t	g_mutex_n_finished;
-pthread_mutex_t	g_mutex_write;
 
 int				philo_init(pthread_t **philo_pthread, long n_philo);
 void			*philo_activity(void *philo_arg);
-long			philo_eat(t_philo *philo, long time_start_eating);
-int				philo_sleep(t_philo *philo, long time_start_eating);
-int				philo_check_dead(t_philo *philo, long time_start_eating);
-int				philo_free_ret(t_philo *philo, pthread_t *philo_pthread,
+long			philo_eat(t_lst *lst, long time_start_eating);
+int				philo_sleep(t_lst *lst, long time_start_eating);
+int				philo_check_dead(t_lst *lst, long time_start_eating);
+int				philo_free_ret(t_lst *lst, pthread_t *philo_pthread,
 																	int ret);
 void			philo_mutex_destroy(long n_philo);
-int				philo_store_argv(int argc, char **argv, t_philo **philo);
+int				ft_save_argv(int argc, char **argv, t_lst **lst);
 long			philo_gettime(void);
 void			philo_putstatus(long philo_index, long time_ms,
 														int philo_status);
 int				philo_has_finished(long time_start, long time_to_do);
-int				philo_puterr_and_return(const char *str, int ret);
+int	ft_put_error(const char *str, int ret);
 
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 int				ft_isspace(int c);
 int				ft_isdigit(int c);
 int				ft_atol(const char *str);
