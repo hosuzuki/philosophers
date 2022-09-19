49e1ba23863098ea732985aca74e4245af99c51c
hosuzuki <hokuto.suzuki@gmail.com>
2022-09-19 17:20:01 +0900

    delete test function

--- philo/start_simulation.c
+++ philo/start_simulation.c
@@ -6,49 +6,31 @@
 /*   By: hos <hosuzuki@student.42tokyo.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 14:39:02 by hos               #+#    #+#             */
-/*   Updated: 2022/09/19 14:42:17 by hos              ###   ########.fr       */
+/*   Updated: 2022/09/19 16:58:04 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-char *test(t_lst *l, char *s)
-{
-	printf("index: %ld - Status: %s\n", l->index, s);
-	return (NULL);
-}
-
 void	*life_of_philo(void *arg)
 {
 	t_lst	*l;
-	long	time_think;
 
 	l = (t_lst *)arg;
 	if (l->index % 2)
 		usleep (200);
 	pthread_mutex_lock(l->philo);
 	l->last_meal = what_time();
-	if (l->last_meal < 0)
-	{
-		pthread_mutex_unlock(l->philo);
-		return (NULL);
-	}
 	pthread_mutex_unlock(l->philo);
 	activate_death_watcher(l);
 	while (1)
 	{
 		if (eat_task(l) < 0)
-			//return (NULL);
-			return (test(l, "eat"));// delete this later.
-		if (sleep_task(l) < 0)
-			//return (NULL);
-			return (test(l, "sleep"));
-		time_think = what_time();
-		if (time_think < 0)
 			return (NULL);
-		if (put_status(l, time_think, THINKING) < 0)
-			//return (NULL);
-			return (test(l, "think"));
+		if (sleep_task(l) < 0)
+			return (NULL);
+		if (put_status(l, what_time(), THINKING) < 0)
+			return (NULL);
 	}
 	return (NULL);
 }
