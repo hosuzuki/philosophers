29b4ecbc7109868ae63b1c0b254e55c9bfbe2381
hosuzuki <hokuto.suzuki@gmail.com>
2022-09-19 20:47:50 +0900

    before major change

--- philo_bonus/init_lst.c
+++ philo_bonus/init_lst.c
@@ -6,7 +6,7 @@
 /*   By: hos <hosuzuki@student.42tokyo.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 14:39:02 by hos               #+#    #+#             */
-/*   Updated: 2022/09/14 23:35:52 by hos              ###   ########.fr       */
+/*   Updated: 2022/09/19 20:12:23 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 static int	activate_sem(t_sem *sem, long num_philo)
 {
-	sem_unlink(SEM_NAME);
-	sem_unlink(SEM_WRITE);
+	sem_unlink(SEM_FORKS);
+	sem_unlink(SEM_WRITER);
+	sem_unlink(SEM_MEAL_FLAGS);
 	errno = 0;
-	sem->fork = sem_open(SEM_NAME, O_CREAT | O_EXCL, 777, num_philo);
-	sem->sem_write = sem_open(SEM_WRITE, O_CREAT | O_EXCL, 777, 1);
+	sem->forks = sem_open(SEM_FORKS, O_CREAT | O_EXCL, 777, num_philo);
+	sem->writer = sem_open(SEM_WRITER, O_CREAT | O_EXCL, 777, 1);
+	sem->meal_flags = sem_open(SEM_MEAL_FLAGS, O_CREAT | O_EXCL, 777, num_philo);
 	if (errno != 0)
 		put_error_and_exit("sem_open", -1);
 	return (0);
@@ -47,6 +49,8 @@ int	init_lst(t_lst **l, t_info *info)
 	{
 		(*l)[i].index = i + 1;
 		(*l)[i].last_meal = 0;
+		(*l)[i].meal_flag = &(sem->meal_flags[i]);
+		printf("meal_flag: %p\n", &(sem->meal_flags[i]));
 		(*l)[i].info = info;
 		(*l)[i++].sem = sem;
 	}
