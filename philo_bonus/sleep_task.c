0f63f8531cd33414e813fd36bf394864a75043df
hosuzuki <hokuto.suzuki@gmail.com>
2022-09-19 22:36:20 +0900

    type

--- philo_bonus/sleep_task.c
+++ philo_bonus/sleep_task.c
@@ -6,7 +6,7 @@
 /*   By: hos <hosuzuki@student.42tokyo.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 14:39:03 by hos               #+#    #+#             */
-/*   Updated: 2022/09/14 23:32:57 by hos              ###   ########.fr       */
+/*   Updated: 2022/09/19 18:00:08 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int	sleep_task(t_lst *l)
 	long	now;
 
 	now = what_time();
-	sem_wait(l->sem->sem_write);
+	sem_wait(l->sem->writer);
 	printf("%ld %ld is sleeping\n", now, l->index);
-	sem_post(l->sem->sem_write);
+	sem_post(l->sem->writer);
 	while (!task_is_finished(now, l->info->ms_sleep))
 		usleep(INTERVAL);
 	return (0);
