ccf71804df77415dc861f2aaf0563f4c3143b8bd
hosuzuki <hokuto.suzuki@gmail.com>
2022-09-19 17:19:45 +0900

    deleted free_all.c

--- philo/utils.c
+++ philo/utils.c
@@ -6,11 +6,31 @@
 /*   By: hos <hosuzuki@student.42tokyo.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 14:39:02 by hos               #+#    #+#             */
-/*   Updated: 2022/09/19 12:56:14 by hos              ###   ########.fr       */
+/*   Updated: 2022/09/19 17:16:28 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+int	free_all(t_info *info, t_flags *flags, t_data *data, t_lst *l)
+{
+	if (info)
+		free (info);
+	if (flags)
+	{
+		free(flags->num_to_eat_flags);
+		free (flags);
+	}
+	if (data)
+	{
+		free (data->philos);
+		free (data->forks);
+		free (data);
+	}
+	if (l != NULL)
+		free (l);
+	return (-1);
+}
 
 long	what_time(void)
 {
